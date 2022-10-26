b607b1748a9c87f198e8b620ef7231d8fac531f0
derek <harrison.derek.w@gmail.com>
2022-10-26 15:23:05 +0200

    refactor shortest path with alternating colors solution

--- 1129. Shortest Path with Alternating Colors/1129-shortest-alternating-path/Solution.cpp
+++ 1129. Shortest Path with Alternating Colors/1129-shortest-alternating-path/Solution.cpp
@@ -11,78 +11,88 @@
 
 using namespace std;
 
+const int LARGE = 3e8;
+
+typedef enum Color{
+    RED = 1,
+    BLUE = 2,
+    GREEN = 3,
+    DEF = 0
+} cl;
+
 typedef struct Node {
     int index;
     int dist;
-    int list;
+    cl color;
 } nd;
-
-const int LARGE = 3e8;
 
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>> & blueEdges) {
         
-        vector<vector<int>> colorMat(n, vector<int>(n, -n));
+        vector<vector<cl>> colorMat(n, vector<cl>(n, DEF));
         vector<vector<int>> adjList(n, vector<int>(0, 0));
         
         // Make adjacency list and color matrix
         for(auto v : redEdges) {
             int i = v[0];
             int j = v[1];
-            colorMat[i][j] = 1;
+            colorMat[i][j] = RED;
             adjList[i].push_back(j);
         }
         
         for(auto v : blueEdges) {
             int i = v[0];
             int j = v[1];
-            if(colorMat[i][j] == 1) {
-                colorMat[i][j] = 0;
+            if(colorMat[i][j] == RED) {
+                colorMat[i][j] = GREEN;
             }
-            else {
-                colorMat[i][j] = -1;
+            if(colorMat[i][j] == DEF) {
+                colorMat[i][j] = BLUE;
                 adjList[i].push_back(j);
             }
         }
                 
-        vector<vector<bool>> visited(n, vector<bool>(3, false));
+        // Perform breadth-first search
         vector<int> res(n, LARGE);
+                
+        vector<vector<bool>> visited(n, vector<bool>(4, false));
         
         queue<nd> queue;
         
-        queue.push({0, 0, 1});
-        queue.push({0, 0, -1});
+        queue.push({0, 0, BLUE});
+        queue.push({0, 0, RED});
         
-        // BFS
         while(!queue.empty()) {
-            int size = (int) queue.size();
+            int size = queue.size();
             
-            for(int k = 0; k < size; k++) {
-                nd node = queue.front();
-                queue.pop();
-                
-                int dist = node.dist;
-                int index = node.index;
-                int list = node.list;
-                
-                if(visited[index][list + 1])
+            nd node = queue.front();
+            queue.pop();
+
+            int dist = node.dist;
+            int index = node.index;
+            cl color = node.color;
+
+            if(visited[index][color])
+                continue;
+
+            visited[index][color] = true;
+
+            // Compute distance from node 0 to index
+            res[index] = min(res[index], dist);
+
+            // Explore neighbors
+            for(auto e : adjList[index]) {
+                cl col = colorMat[index][e];
+
+                if(visited[e][col])
                     continue;
-                
-                visited[index][list + 1] = true;
-                
-                // Compute distance from node 0 to index
-                res[index] = min(res[index], dist);
-                
-                // Explore neighbors
-                for(auto e : adjList[index]) {
-                    int col = colorMat[index][e];
-                    if(visited[e][col + 1]) continue;
-                    // Add neighbors to queue
-                    if(-list == col || col == 0) {
-                        queue.push({e, dist + 1, -list});
-                    }
-                }
+
+                // Add neighbors to queue
+                if(color == RED && (col == GREEN || col == BLUE))
+                    queue.push({e, dist + 1, BLUE});
+                if(color == BLUE && (col == GREEN || col == RED))
+                    queue.push({e, dist + 1, RED});
             }
         }
         
